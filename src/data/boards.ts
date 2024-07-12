8eb259103b691c174ac661dcec58f667080cc030
ryou <ryo@example.com>
2024-07-12 22:36:08 +0900

    add board type, data

---
+++ src/data/boards.ts
@@ -0,0 +1,19 @@
+import type { Board } from "domains/types";
+
+export const boardData: Board[] = [
+  {
+    id: 1,
+    title: "board1",
+    contents: "this is board1",
+  },
+  {
+    id: 2,
+    title: "board2",
+    contents: "this is board2",
+  },
+  {
+    id: 3,
+    title: "board3",
+    contents: "this is board3",
+  },
+];
