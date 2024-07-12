8e1cce299478f57248300631aa915b5e6e607335
ryou <ryo@example.com>
2024-07-13 05:02:42 +0900

    WIP dragging board

--- src/features/Boards/components/Boards.tsx
+++ src/features/Boards/components/Boards.tsx
@@ -1,13 +1,31 @@
+import { useState } from 'react';
+import { DndContext, DragOverlay } from '@dnd-kit/core';
+import { rectSortingStrategy, SortableContext } from '@dnd-kit/sortable';
 import BoardList from 'features/Boards/components/BoardList';
 
 import { boardData } from 'data/boards';
+import { Board } from 'domains/types';
 
 const Boards = () => {
+  const [boards, setBoards] = useState<Board[]>(boardData);
+
+  const handleDragEnd = () => {
+    console.log("Druuuuuuuuug!!");
+  }
+
   return (
-    <div>
+    <DndContext onDragEnd={handleDragEnd}>
       <h1 className='text-4xl mb-4 text-white'>Your Board</h1>
-      <BoardList boards={boardData} />
-    </div>
+      <SortableContext items={boards} strategy={rectSortingStrategy}>
+        <div className='mx-6 my-6 gap-6 grid md:grid-cols-6 lg:grid-col-5'>
+          {boardData.length > 0 && boardData.map(board => (
+            <BoardList key={board.id} board={board} id={board.id} />
+          ))}
+        </div>
+      </SortableContext>
+      <DragOverlay>
+      </DragOverlay>
+    </DndContext>
   );
 };
 
