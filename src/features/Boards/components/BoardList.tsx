32a517a6417284d65fc2ee760465caf90cdb2ada
ryou <ryo@example.com>
2024-07-12 23:28:55 +0900

    fix style

--- src/features/Boards/components/BoardList.tsx
+++ src/features/Boards/components/BoardList.tsx
@@ -7,10 +7,9 @@ type Props = {
 
 const BoardList = ({ boards }: Props) => {
   return (
-    // <div className="mx-5 my-5 gap-5 grid md:grid-col-3 lg:grid-col-5">
-    <div className='mx-5 my-5 gap-5 grid md:grid-cols-6 lg:grid-col-5'>
+    <div className='mx-6 my-6 gap-6 grid md:grid-cols-6 lg:grid-col-5'>
       {boards?.length > 0 && boards.map(board => (
-        <div key={board.id} className='relative px-8 py-12 block rounded-md bg-gray-400'>
+        <div key={board.id} className='px-8 py-12 font-semibold block rounded-md bg-gray-400'>
           <Link to={`/boards/${board.id}`}>
             {board.title}
           </Link>
