a3990fc7b081caafc27d6cefbdefffa42ec43f63
ryou <ryo@example.com>
2024-07-13 22:13:14 +0900

    add can drag & drop board

--- src/features/Boards/components/Boards.tsx
+++ src/features/Boards/components/Boards.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
-import { DndContext, DragOverlay } from '@dnd-kit/core';
-import { rectSortingStrategy, SortableContext } from '@dnd-kit/sortable';
+import { DndContext, DragEndEvent } from '@dnd-kit/core';
+import { arrayMove, rectSortingStrategy, SortableContext } from '@dnd-kit/sortable';
 import BoardList from 'features/Boards/components/BoardList';
 
 import { boardData } from 'data/boards';
@@ -9,22 +9,32 @@ import { Board } from 'domains/types';
 const Boards = () => {
   const [boards, setBoards] = useState<Board[]>(boardData);
 
-  const handleDragEnd = () => {
-    console.log("Druuuuuuuuug!!");
-  }
+  const handleDragEnd = (ev: DragEndEvent) => {
+    const { active, over } = ev;
+    if (over === null) {
+      return null;
+    }
+
+    if (active.id !== over.id) {
+      setBoards(boards => {
+        const oldBoardId = boards.findIndex(board => board.id === active.id);
+        const newBoardId = boards.findIndex(board => board.id === over.id);
+        return arrayMove(boards, oldBoardId, newBoardId);
+      });
+    }
+  };
 
   return (
-    <DndContext onDragEnd={handleDragEnd}>
-      <h1 className='text-4xl mb-4 text-white'>Your Board</h1>
+    <DndContext
+      onDragEnd={handleDragEnd}
+    >
       <SortableContext items={boards} strategy={rectSortingStrategy}>
-        <div className='mx-6 my-6 gap-6 grid md:grid-cols-6 lg:grid-col-5'>
-          {boardData.length > 0 && boardData.map(board => (
+        <div className='mx-6 my-6 gap-6 grid md:grid-cols-5 lg:grid-col-5'>
+          {boards.length > 0 && boards.map(board => (
             <BoardList key={board.id} board={board} id={board.id} />
           ))}
         </div>
       </SortableContext>
-      <DragOverlay>
-      </DragOverlay>
     </DndContext>
   );
 };
