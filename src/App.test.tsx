2bc2cb496721a73b4367cf941dfb4c36872ab7b2
rm-ryou <emco.420118@gmail.com>
2024-09-13 02:42:37 +0900

    add todo domain

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,16 +1,16 @@
-import { expect, test } from 'vitest'
-import { render, screen } from '@testing-library/react'
-import userEvent from '@testing-library/user-event'
-import '@testing-library/jest-dom'
+import { expect, test } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import '@testing-library/jest-dom';
 
-import App from './App'
+import App from './App';
 
 test('クリックするとカウントが1増える', async () => {
   const user = userEvent.setup();
-  render(<App />)
+  render(<App />);
 
-  const countButton = screen.getByText('count is 0')
-  await user.click(countButton)
+  const countButton = screen.getByText('count is 0');
+  await user.click(countButton);
 
-  expect(countButton).toHaveTextContent('count is 1')
-})
+  expect(countButton).toHaveTextContent('count is 1');
+});
