38a927f0b08bcd6fe2db9aaa2d852d28f591d6b6
rm-ryou <emco.420118@gmail.com>
2024-09-13 02:36:49 +0900

    add first test

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,18 @@
-import { defineConfig } from 'vite';
+/// <reference type="vitest" />
+/// <reference type="vite/client" />
+
+import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 import viteTsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), viteTsconfigPaths()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    coverage: {
+      provider: 'v8'
+    },
+  },
 });
