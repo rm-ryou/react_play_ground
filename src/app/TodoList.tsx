4add0628388216b7e9aba59fa964d430b428c4e9
rm-ryou <emco.420118@gmail.com>
2024-09-14 19:00:50 +0900

    add outlet for router

--- src/app/TodoList.tsx
+++ src/app/TodoList.tsx
@@ -1,10 +1,11 @@
-// import { Outlet } from "react-router-dom"
+import { Outlet } from "react-router-dom"
 
 import TodoListContainer from '@/features/todos/components/TodoListContainer';
 
 const TodoList = () => {
   return (
     <>
+      <Outlet />
       <main>
         <TodoListContainer />
       </main>
